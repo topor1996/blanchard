0b82458ccce816938f12b534721471dbb1ff71a2
T1 <andrey.toporovsky@mail.ru>
2021-06-21 05:05:01 +0300

    21.06

---
+++ 16_knowledge-base/js/tabs.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoader', function () {
+  document.querySelectorAll('.artist').forEach(function (tabsBtn) {
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+      document.querySelectorAll ('.catalog-left-block').forEach(function(tabContent){
+        tabContent.classList.remove('.catalog-left-block-active')
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('.catalog-left-block-active')
+    })
+  })
+})
+
+
+
+
+
+
